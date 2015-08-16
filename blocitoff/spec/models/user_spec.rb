b64dece3bd8a5ad463a147ee8ca0e19cf60d2888
Jason Quaccia <jlquaccia@gmail.com>
2015-08-16 10:46:58 -0700

    added rspec and stubbed out a user_spec.rb file

---
+++ blocitoff/spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe User do
+  include TestFactories
+
+  describe "user sign up" do
+    xit "signs a new user up" do
+
+    end
+
+    xit "sends out a confirmation email" do
+
+    end
+
+    xit "prevents the use of invalid emails" do
+
+    end
+
+    xit "prevents the use of duplicate emails" do
+
+    end
+  end
+end
